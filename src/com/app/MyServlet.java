2a700a56a1a0ef6ba85c964b9cc6684b4ec39aca
Amlesh-Singh <amlesh5001@gmail.com>
2017-09-30 09:54:39 +0530

    new

--- src/com/app/MyServlet.java
+++ src/com/app/MyServlet.java
@@ -20,6 +20,7 @@ public class MyServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 PrintWriter out = response.getWriter();
 out.println("Hiii");
+out.println("Hello");
 	}
 
 }
