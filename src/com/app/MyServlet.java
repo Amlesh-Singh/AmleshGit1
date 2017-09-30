4d5d0636d7fecbc90e898b482d76f16cecccd85a
Amlesh-Singh <amlesh5001@gmail.com>
2017-09-30 09:54:02 +0530

    my Hello

---
+++ src/com/app/MyServlet.java
@@ -0,0 +1,25 @@
+package com.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MyServlet extends HttpServlet {
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+PrintWriter out = response.getWriter();
+out.println("Hiii");
+	}
+
+}
